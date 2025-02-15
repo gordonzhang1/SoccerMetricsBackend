70c05aa4f1e0403af4a49821d1e04812585f942d
Gordon06060 <gordonz0913@gmail.com>
2025-02-14 21:25:29 -0500

    fix: port changes

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def get_ball():
     
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.getenv('PORT', 5000))  # Default to 5000 if no PORT environment variable is set
+    app.run(debug=True, host='0.0.0.0', port=port)
